0c7e028b54e580ca3a03d3dc4dc3b7a7a97ad3e7
felipesora <felipeusora@gmail.com>
2025-05-12 13:38:44 -0300

    Limpando o projeto

--- mototrack-backend-dotnet/obj/Debug/net8.0/mototrack-backend-dotnet.AssemblyInfo.cs
+++ mototrack-backend-dotnet/obj/Debug/net8.0/mototrack-backend-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d485b1185a9b2b42d683ec33d6ab06d5123ede6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
