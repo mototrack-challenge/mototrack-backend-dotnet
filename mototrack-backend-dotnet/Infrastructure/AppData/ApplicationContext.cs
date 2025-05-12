31376a95d30d154341897600633766c6c1322f0f
felipesora <felipeusora@gmail.com>
2025-05-12 18:09:58 -0300

    Criando ApplicationContext com DbSet de OrdemServico

---
+++ mototrack-backend-dotnet/Infrastructure/AppData/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mototrack_backend_dotnet.Domain.Entities;
+
+namespace mototrack_backend_dotnet.Infrastructure.AppData;
+
+public class ApplicationContext  : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+    }
+
+    DbSet<OrdemServico> OrdemServico { get; set; }
+}
--- mototrack-backend-dotnet/obj/Debug/net8.0/mototrack-backend-dotnet.AssemblyInfo.cs
+++ mototrack-backend-dotnet/obj/Debug/net8.0/mototrack-backend-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53134a11756335646e144ca0cb47aacb1b5c1724")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7592163c59c7b6ad86932ab1401ef500f1b7172")]
 [assembly: System.Reflection.AssemblyProductAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mototrack-backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
